341b2932ce5924daf457cf46ac94cbb24f17388d
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-27 01:15:25 +0100

    Update and rename Bash.sh to Bash.zsh

--- Bash.zsh
+++ Bash.zsh
@@ -1,5 +1,6 @@
-brew install git-xet
-git xet install
-git clone https://huggingface.co/datasets/aidigestorg/ai-village
+$brew install git-xet
+$git xet install
+$git clone https://huggingface.co/datasets/aidigestorg/ai-village
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/datasets/aidigestorg/ai-village
 hf download aidigestorg/ai-village --repo-type=dataset
+$opencode github install
